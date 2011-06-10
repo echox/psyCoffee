c22097bb099a7f6fe75b355acac1fd3dfa722b6a
Simon Koelsch <echox@echox.de>
2011-06-11 01:53:40 +0200

    added some info to the readme

---
+++ README.mk
@@ -0,0 +1,8 @@
+# psyCoffee - a psyc java library
+
+psyCoffee is a java library based on the [syntax specification Version 1.0 alpha](http://about.psyc.eu/Spec:Syntax) of the PSYC protocol.
+
+PSYC is a protocol for a messaging infrastructure for text-based communication, but also enabling transparent binary data.
+More Information on PSYC resides at (http://www.psyc.eu).
+
+**Disclaimer:** psyCoffee is currently under heavy development and not usable.
