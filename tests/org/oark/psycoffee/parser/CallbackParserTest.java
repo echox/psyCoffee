f481b8ffc7fdd79a193172ae02ee5112a9eca5b0
Simon Koelsch <echox@echox.de>
2011-06-12 17:48:13 +0200

    added quicktest for parsing

---
+++ tests/org/oark/psycoffee/parser/CallbackParserTest.java
@@ -0,0 +1,54 @@
+/* 
+ * Copyright (c) 2011, Simon Kölsch
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+package org.oark.psycoffee.parser;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.oark.psycoffee.core.Context;
+import org.oark.psycoffee.core.Packet;
+
+public class CallbackParserTest {
+
+	public static String example = "|\n" 
+		   + ":_source\tpsyc://example.symlynX.com/~fippo\n"
+		   + ":_target\tpsyc://ente.aquarium.example.org:-32872\n"
+		   + "\n"
+		   + ":_nick\tfippo\n"
+		   + "_info_nickname\n"
+		   + "Hello [_nick].\n"
+		   + "\n"
+		   + "more text\n"
+		   + "|";
+	
+	public class cb implements Callback {
+
+		@Override
+		public void parsed(Packet packet, Context context) {
+			
+			assertTrue(example.equals(packet.toString()));
+		}
+		
+	}
+	
+	//TODO remove later
+	@Test
+	public void quicktest() {
+		
+		CallbackParser parser = new CallbackParser();
+		parser.addCallback(new cb());
+		parser.parse(example);
+	}
+}
